f4c49dc82b2683e4869698f0d8d1ccf59f2fb44c
Gowtham Punniyaseelan <88957910+gowthampunniyaseelan@users.noreply.github.com>
2022-08-22 14:19:25 +0530

    added the comments

--- bank_application/customer.py
+++ bank_application/customer.py
@@ -1,27 +1,52 @@
 filename="bank_application/bank_db.txt"
 class Customer:
+  """
+  This function is used to write the new user in the file
+  """
   def __init__(self,customerId,name,balance,password,accountId):
     self.customerId = customerId
     self.name = name
     self.balance = balance
     self.password = password
     self.accountId = accountId
+
+    """
+    This is to write new user in the file
+    """
     with open(filename,"r+") as f:
       data = self.customerId +" "+ self.accountId + " " + self.name + " " + self.balance + " " + self.password+"\n"
       f.seek(0,2)
       f.write(data)
+    """
+    Create new file for every user to track their bank transaction and their history of details about bank balance.
+    """
     with open(f"bank_application/{self.customerId}.txt","w") as  write_new_file:
       pass
+    """
+    Write that user details of transaction in their file
+    """
     with open(f"bank_application/{self.customerId}.txt","r+") as  new_file:
       history_id = 1
       history_data = str(history_id) +" "+ "opening" + " " + self.balance + " " + self.balance + "\n"
       new_file.seek(0,2)
-      new_file.write(history_data) 
+      new_file.write(history_data)
+    """
+    Get the input from the user for to open the account
+    """
   @classmethod
   def getInputFromUser(cls):
+    """
+    Read the existing file from the bank_db.txt
+    """
     with open(filename,"r") as f:
       data=f.readlines()
+    """
+    Loop through the file
+    """
     for i in range(len(data)):
+      """
+      Split the last user in the file and get the data like customerId,accountId and then increase the accountId and customerId to add another user
+      """
       result=data[i].split()
       customerId=int(result[0])
       accountId = int(result[1])
@@ -31,6 +56,9 @@ class Customer:
     balance=int(input("Enter the initial amount must be thousand: "))
     password=input("Enter your password: ")
     repassword=input("Enter your password again: ")
+    """
+    Hashing the password like if the user enter 1 then the hashing password is 2 and if it is 9 means 0.z means a , Z means A
+    """
     if password==repassword:
       ascii_value = [ord(i) for i in password]
       for j in range(len(ascii_value)):
@@ -43,6 +71,9 @@ class Customer:
       new_value = [k+1 for k in ascii_value]
       final_password=[chr(k) for k in new_value]
       password="".join(final_password)
+      """
+      After getting the user details we are going to give that to the constructor.    
+      """
       Customer(str(customerId),name,str(balance),password,str(accountId))
     else:
       print("Enter the password again.")
@@ -51,19 +82,34 @@ class Customer:
     customerId=input("Enter the customer ID: ")
     accountId = input("Enter the account ID: ")
     amount=int(input("Enter the amount that you want to deposite: "))
+    """
+    Reading the file to add the amount
+    """
     with open("bank_application/bank_db.txt","r+") as readfile:
       data=readfile.readlines()
       for i in range(len(data)):
         result=data[i].split()
         cId = result[0]
         aId = result[1]
+        """
+        Using customerId and accountId to find the user
+        """
         if cId == customerId and aId == accountId:
+          """
+          Adding that amount to the user
+          """
           result[3]=str(int(result[3])+amount)
           dummy_result=" ".join(result)
           data[(int(cId)-1)]=dummy_result+"\n"
       original_result="".join(data)
+      """
+      Add the amount to the file
+      """
     with open(filename,"w") as writefile:
       writefile.write(original_result)
+      """
+      Find the seperate user file and add the deposite details
+      """
     with open(f"bank_application/{customerId}.txt","r+") as transaction_history:
       data=transaction_history.readlines()[-1]
       data = data.split()
@@ -78,6 +124,9 @@ class Customer:
       transaction_history.write(end)
   @classmethod
   def withdrawl(cls):
+    """
+    Same as comments in the deposite function but it will substract the money instead of adding it.
+    """
     customerId=input("Enter the customer ID: ")
     accountId = input("Enter the account ID: ")
     amount=int(input("Enter the amount that you want to withdraw: "))
@@ -112,6 +161,17 @@ class Customer:
               transaction_history.write(end)
   @classmethod
   def transfer(cls):
+    """
+    Same as deposite function and withdraw function 
+    but we are going to find the user using the same
+    method in deposite and withdraw function.
+    And then withdraw the amount from the user who are
+    going to transact the amount and then getting
+    the customerId and accountId to transact that amount to
+    the other user and then adding the amount
+    to the user.In this all the operation we are going to keep
+    track of the user individual file and write the transaction details. 
+    """
     print("Enter your details")
     customerId=input("Enter the customer ID: ")
     accountId = input("Enter the account ID: ")
@@ -175,6 +235,10 @@ class Customer:
       transaction_history.write(end)
   @classmethod
   def getTransactionalHistory(cls):
+    """
+    Get the individual user details like CustomerId to get their
+    transaction details from their own file
+    """
     customerId=input("Enter the customer ID: ")
     password=input("Enter your password: ")
     ascii_value = [ord(i) for i in password]
